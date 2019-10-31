34dcb4d7237b8a3232e7250ee554b11c599683dc
mohamm-alsaid <mohamm.alsaid@gmail.com>
2019-10-31 07:57:18 -0700

    new wireframe

--- frontend/webapp.py
+++ frontend/webapp.py
@@ -1,30 +1,46 @@
-import request
+import requests
+import secrets
 import urllib
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = secrets.token_urlsafe(16)
 
 # routing home page
 @app.route('/')
-@app.route('/home')
 def home():
-    return render_template('home.html', title='home')
+    return render_template('layout.html')
 
-# routing about page
-@app.route('/about')
-def about():
-    return render_template('about.html', title='about')
+# routing register page
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if(request.method=='POST'):
+        addr=request.form['addr_server']
+        pin=request.form['pin']
+        session['addr']=addr
+        session['pin']=pin
+        return redirect(url_for('settings'))
+    
+    return render_template('register.html')
 
-# routing contact page
-@app.route('/contact')
-def contact():
-    return render_template('contact.html', title='contact')
 
-# routing login
-@app.route('/login')
-#@app.route('/login', methods=['POST'])
-def login():
-    return render_template('login.html',title='login')
+# routing settings page
+@app.route('/settings', methods=['GET','POST'])
+def settings():
+    if request.method=='POST':
+        val = request.form['participation']
+        print(val)
+        print(session['pin'])
+        print(session['addr'])
+        return redirect(url_for('status'))
+    return render_template('settings.html')
+
+# routing status page
+@app.route('/status')
+def status():
+    # will graph the status of the grid?
+    return render_template('status.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
